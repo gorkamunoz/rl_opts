8cc15cdc80805d72a2e3f614bd44178bbd98417b
Gorka Muñoz <munoz.gil.gorka@gmail.com>
2026-04-28 14:34:18 +0200

    Create launch_exp_12.py

---
+++ nbs/lib_nbs/develop/launch_exp_12.py
@@ -0,0 +1,94 @@
+from rl_opts.rl_framework.numba.agents import Foragers_efficient, run_collective
+from rl_opts.rl_framework.numba.environments import CollectiveEnv
+import numba
+import os
+import numpy as np
+import socket
+
+num_agents = 50
+max_counter = 50
+runs = 1
+
+Nt = 100; 
+L = 100 # New larger environment, smaller density
+r = 0.5
+agent_step = 1
+# Time delays
+
+tau = 11 # To promote non-local strategies
+taus_reward = [1,2]
+
+# Visual cone
+visual_activated = True
+visual_range = 2.0
+visual_angles = np.pi / np.arange(2,8,2) # Note that this is from the center, so from +45 to -45
+
+# Training parameters
+gamma_damping = 0.00001
+eta_glow_damping = 0.1
+# State space: [counter, any_agent_in_cone (0/1), rewarded_agent_in_cone (0/1)]
+state_space = np.array([max_counter, 2, 2])
+
+time_ep = 5000
+episodes = 500
+
+# Saving data
+if os.getlogin() == "gorka":
+    if socket.gethostname() == "gpu-qic":
+        out_dir = "/media/gorka/DATA/rl_opts_data/results_learning/collective/"
+    elif socket.gethostname() == "gpu-ada-qic":
+        out_dir = "/home/gorka/rl_opts/nbs/lib_nbs/develop/results/"
+elif os.getlogin() == "c7051165":
+    out_dir = "/scratch/c7051165/github/rl_opts/nbs/lib_nbs/develop/results/"
+
+filename = os.path.basename(__file__)  
+exp = filename.replace("launch_exp_", "").replace(".py", "")
+out_dir = os.path.join(out_dir, f"exp_{exp}")
+os.makedirs(out_dir, exist_ok=True)
+# Test save
+np.save(os.path.join(out_dir, 'test_save.npy'), np.arange(10))
+
+shared_depletion = False
+
+for tau_reward in taus_reward:
+    for visual_angle in visual_angles:
+
+        file_name = f"_shared_{shared_depletion}_tau_reward_{tau_reward}_visual_angle_{visual_angle}.npy"
+        rewards_path = os.path.join(out_dir, 
+                                    "rewards"+file_name)
+        h_matrix_path = os.path.join(out_dir, 
+                                     f"h_matrix"+file_name)
+
+
+        if os.path.exists(rewards_path) and os.path.exists(h_matrix_path):
+            print(f"Skipping existing case: {file_name}")
+            continue
+    
+        
+    
+        rews, mats = run_collective(episodes = episodes, time_ep = time_ep, 
+                                    runs=runs * numba.get_num_threads(),
+                                    # Environment props
+                                    Nt=Nt,
+                                    L=L,
+                                    r=r,
+                                    tau=tau,
+                                    tau_reward=tau_reward,
+                                    num_agents=num_agents,
+                                    agent_step=agent_step,
+                                    visual_range=visual_range,
+                                    visual_angle=visual_angle,
+                                    shared_depletion=shared_depletion,
+                                    visual_activated = visual_activated,
+                                    # Agent props
+                                    num_actions = 2, s
+                                    state_space=state_space,
+                                    gamma_damping=gamma_damping,
+                                    eta_glow_damping=eta_glow_damping,
+                                    upd_pos_method = 'RND'
+                                    )
+        
+        
+    
+        np.save(rewards_path, rews)
+        np.save(h_matrix_path, mats)
