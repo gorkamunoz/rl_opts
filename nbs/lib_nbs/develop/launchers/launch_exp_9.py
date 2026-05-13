9920cdb2942c688f275ba96306968acdfa208551
Gorka <munoz.gil.gorka@gmail.com>
2026-05-13 08:07:13 +0200

    adapted launcher for rerun

--- nbs/lib_nbs/develop/launchers/launch_exp_9.py
+++ nbs/lib_nbs/develop/launchers/launch_exp_9.py
@@ -3,6 +3,7 @@ from rl_opts.rl_framework.numba.environments import CollectiveEnv
 import numba
 import os
 import numpy as np
+import socket
 
 num_agents = 50
 max_counter = 50
@@ -12,7 +13,7 @@ Nt = 100;
 L = 50; r = 0.5
 agent_step = 1
 # Time delays
-taus = np.arange(2, 12)
+taus = np.arange(2, 12)[4:]
 tau_reward = 2
 # Visual cone
 visual_activated = True
@@ -30,7 +31,10 @@ episodes = 2000
 
 # Saving data
 if os.getlogin() == "gorka":
-    out_dir = "/media/gorka/DATA/rl_opts_data/results_learning/collective/"
+    if socket.gethostname() == "gpu-qic":
+        out_dir = "/media/gorka/DATA/rl_opts_data/results_learning/collective/"
+    elif socket.gethostname() == "gpu-ada-qic":
+        out_dir = "/home/gorka/rl_opts/nbs/lib_nbs/develop/results/"
 elif os.getlogin() == "c7051165":
     out_dir = "/scratch/c7051165/github/rl_opts/nbs/lib_nbs/develop/results/"
 
@@ -38,9 +42,14 @@ filename = os.path.basename(__file__)
 exp = filename.replace("launch_exp_", "").replace(".py", "")
 out_dir = os.path.join(out_dir, f"exp_{exp}")
 os.makedirs(out_dir, exist_ok=True)
+# Test save
+np.save(os.path.join(out_dir, 'test_save.npy'), np.arange(10))
 
 for tau in taus:
     for shared_depletion in [True, False]:
+
+        if tau == taus[0] and shared_depletion:
+            continue
 
         file_name = f"_shared_{shared_depletion}_tau_{tau}.npy"
         rewards_path = os.path.join(out_dir, 
@@ -52,6 +61,8 @@ for tau in taus:
         if os.path.exists(rewards_path) and os.path.exists(h_matrix_path):
             print(f"Skipping existing case: {file_name}")
             continue
+
+        print(f"Running case: {file_name}")
     
         
     
